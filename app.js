34accf6b106b78b1a78605082f0ece1dc004e260
Yann LEVEILLE-MENEZ <yann.leveille-menez@vitalliance.fr>
2020-12-02 06:05:11 +0100

    make it better

--- app.js
+++ app.js
@@ -5,8 +5,10 @@ const db = require('monk')('localhost/requestsLogs');
 app.use(express.json());
 
 app.get('/', (req, res) => {
-    
+
     return res.json({
         message: `Request saving success.`
     });
 });
+
+module.exports = app;
