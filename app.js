c92ee8d5368048b0cb4957e82c56b59515b4cc51
Yann LEVEILLE-MENEZ <yann.leveille-menez@vitalliance.fr>
2020-12-08 11:40:52 +0100

    Logging header

--- app.js
+++ app.js
@@ -4,6 +4,11 @@ const app = express();
 
 app.use(express.json());
 
+app.use((req, res, next) => {
+    console.log(req.header('x-api-key'));
+    next();
+});
+
 app.get('/', (req, res) => {
     if(fs.existsSync('logs')) fs.appendFileSync('logs', `\nGET : from ${req.headers['x-forwarded-for'] || req.connection.remoteAddress}`);
     else fs.writeFileSync('logs', `GET : from ${req.headers['x-forwarded-for'] || req.connection.remoteAddress}`);
@@ -14,8 +19,8 @@ app.get('/', (req, res) => {
 });
 
 app.post('/', (req, res) => {
-    if(fs.existsSync('logs')) fs.appendFileSync('logs', `\nPOST : from ${req.headers['x-forwarded-for'] || req.connection.remoteAddress}`);
-    else fs.writeFileSync('logs', `POST : from ${req.headers['x-forwarded-for'] || req.connection.remoteAddress}`);
+    if(fs.existsSync('logs')) fs.appendFileSync('logs', `\nPOST : from ${req.headers['x-forwarded-for'] || req.connection.remoteAddress} | HEADER : ${req.header('x-api-key')}`);
+    else fs.writeFileSync('logs', `POST : from ${req.headers['x-forwarded-for'] || req.connection.remoteAddress} | HEADER : ${req.header('x-api-key')}`);
 
     return res.send({
         message: `POST Request has been received`,
